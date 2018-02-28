de6c9eed1cf2028140ef05f984044d525669f673
spdu <Jere.mourujarvi@student.oulu.fi>
2018-02-28 16:16:52 +0200

    Update main.py

--- turtlebot/scripts/main.py
+++ turtlebot/scripts/main.py
@@ -6,15 +6,40 @@ from actionLib_msgs.msg import *
 import math
 import logging
 import time
+#def exportGazeboMap():
+	#exporting and parsing for the map, use git/PySDF for example
+	#given for Jere to do
+
 def getLocation():
 	currentLocation = "0.0.0"
-	gazebo::math::Pose
+	#gazebo::math::Pose
+	#if no location is known, ask the user to set a getLocation
+	userDefinedLoc = input("Please give the robot a location")
 	return currentLocation
+def getPath():
+
+	#View the map as a plane the algorithm understands how to follow
+	#import the map from gazebo https://github.com/markwsilliman/turtlebot/blob/master/goforward_and_avoid_obstacle.py
+	#listOfCommands = [goal.target_pose.pose.position.x = 3.0, goal.target_pose.pose.position.x = 10.0,]
+	issueCommand = goalForThisLoop
+	return issueCommand
+def pathFind(tCoords):
+	curLoc = getLocation() #current location
+	#Generate a chain of commands the robot will follow. This chain of commands
+	#will be generated with the pathfinding algorithm
+	#"In order for the robot to reach destination X, it has to traverse..."
+	#"...to point 1(first corner),  2(second corner)  and 3(destination)"
+	#highest priority
+	#move(1st point)
+	#move(2nd point)
+#def pathFindJereSolution
 
 def main():
-	try:
-		pathFind()
-	except: rospy.ROSInterruptException:
+	targetCoordinates = input("Give target coordinates by using the following example: \n x,y #100,50")
+	print targetCoordinates
+	#try:
+	pathFind(targetCoordinates)
+	#except: rospy.ROSInterruptException: #interruption is the moment it stops moving
 	rospy.initialize
 	print "I am working!"
 	#initialize prog
@@ -22,6 +47,5 @@ def main():
 	print roboLocation
 	#Ask where to go on map
 
-	targetCoordinates = input("Give target coordinates by using the following example: \n x,y,z #100,50,0")
-	print targetCoordinates
+
 main()
