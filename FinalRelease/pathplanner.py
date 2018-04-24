e8eddf2a28d2fb01f0ed8ba98fefbf24c8356946
aghaznaw <32619749+aghaznaw@users.noreply.github.com>
2018-04-24 15:10:23 +0300

    Update pathplanner.py

--- FinalRelease/pathplanner.py
+++ FinalRelease/pathplanner.py
@@ -1,8 +1,4 @@
-# Sample code from https://www.redblobgames.com/pathfinding/a-star/
-# Copyright 2014 Red Blob Games <redblobgames@gmail.com>
-#
-# Feel free to use this code in your own projects, including commercial projects
-# License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+
 import math
 class getGazeboMap:
     def loadMap(name):
